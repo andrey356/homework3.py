73a818e124df54c200487510cb34ef587c376f89
andrey356 <and.maschjanov@yandex.ru>
2024-06-10 10:19:37 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+name = "John"
+print(name, type(name))
+age = 25
+print(age, type(age))
+new_age = 26
+print(new_age, type(new_age))
+print(new_age - age)
+is_student = "true"
+print(is_student, type(is_student))
